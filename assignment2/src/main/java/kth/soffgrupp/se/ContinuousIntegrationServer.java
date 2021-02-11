4ce9e1bb50e31f3f3cec745049f505e8d7beca1e
Christian Stjernberg <christian@stjernberg.com>
2021-02-11 01:03:49 +0100

    Add method to update commit status (#11)
    Functionality added to create commit statuses.
    This resolves #6

--- assignment2/src/main/java/kth/soffgrupp/se/ContinuousIntegrationServer.java
+++ assignment2/src/main/java/kth/soffgrupp/se/ContinuousIntegrationServer.java
@@ -3,24 +3,61 @@ package kth.soffgrupp.se;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
 import java.io.IOException;
- 
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-/** 
+import org.kohsuke.github.*;
+
+/**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
  See the Jetty documentation for API documentation of those classes.
 */
 public class ContinuousIntegrationServer extends AbstractHandler
 {
-    
+    GHRepository repo;
+
+    public ContinuousIntegrationServer() {
+        try {
+            GitHub git_api = GitHubBuilder.fromPropertyFile().build();
+
+
+            //assert git_api.isCredentialValid() == true;
+            repo = git_api.getRepository​("soffgrupp6/assignment2");
+
+
+        } catch(java.io.IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    /**
+     * Set commit status for commit sha.
+     *
+     * Status is GHCommitState.ERROR
+     *           GHCommitState.PENDING
+     *           GHCommitState.FAILURE
+     *           GHCommitState.SUCCESS
+     *
+     * @param sha
+     * @param description
+     * @param status
+     */
+    private void setCommitStatus(String sha, String description, GHCommitState status) {
+        try {
+            repo.createCommitStatus(sha, status, "", description, "CI Server");
+        } catch (java.io.IOException e) {
+            System.err.println(e);
+        }
+    }
+
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
-                       HttpServletResponse response) 
+                       HttpServletResponse response)
         throws IOException, ServletException
     {
         String branch = "";
@@ -57,12 +94,12 @@ public class ContinuousIntegrationServer extends AbstractHandler
         response.getWriter().println();
 
     }
- 
+
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
         Server server = new Server(8080);
-        server.setHandler(new ContinuousIntegrationServer()); 
+        server.setHandler(new ContinuousIntegrationServer());
         server.start();
         server.join();
     }
