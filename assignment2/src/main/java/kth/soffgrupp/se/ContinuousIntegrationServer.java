3cba75d7d8f1e54141e712e0bd3d628e1f171f4e
Christian Stjernberg <christian@stjernberg.com>
2021-02-15 00:16:40 +0100

    Separate POST request and GET request handlers (#18)
    * Add method to update commit status
    
    Functionality added to create commit statuses.
    This resolves #6
    
    * Improve comments in ContinuousIntegrationServer
    
    This resolves #16
    
    * Separate POST request and GET request handlers
    
    There was also some simple HTML placeholder added.
    Resolves #14
    
    Co-authored-by: it4e <it4e@users.noreply.github.com>

--- assignment2/src/main/java/kth/soffgrupp/se/ContinuousIntegrationServer.java
+++ assignment2/src/main/java/kth/soffgrupp/se/ContinuousIntegrationServer.java
@@ -9,7 +9,6 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import org.kohsuke.github.*;
 
 /**
@@ -23,11 +22,8 @@ public class ContinuousIntegrationServer extends AbstractHandler
     public ContinuousIntegrationServer() {
         try {
             GitHub git_api = GitHubBuilder.fromPropertyFile().build();
-
-
             //assert git_api.isCredentialValid() == true;
             repo = git_api.getRepository​("soffgrupp6/assignment2");
-
 
         } catch(java.io.IOException e) {
             System.err.println(e);
@@ -55,13 +51,13 @@ public class ContinuousIntegrationServer extends AbstractHandler
     }
 
     /**
-     * This function is called for all incoming requests to the server
-     *
+     * This function handles a POST request. This is run
+     * when there is an incoming webhook from GitHub.
      */
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
+    private void handlePost(String target,
+                            Request baseRequest,
+                            HttpServletRequest request,
+                            HttpServletResponse response)
         throws IOException, ServletException
     {
         GitHandler git;
@@ -95,19 +91,62 @@ public class ContinuousIntegrationServer extends AbstractHandler
         }
         
         git.clean();
-        
-        // Log results
-        // .....
 
-        // Send repsonse. This is probably something Git will use to set branch status e.t.c.
+        // Send response
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        System.out.print("POST: ");
+        System.out.println(target);
+    }
+
+    /**
+     * This function handles a GET request. If the site is requested
+     * in a browser it is served using this function.
+     */
+    private void handleGet(String target,
+                            Request baseRequest,
+                            HttpServletRequest request,
+                            HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        if (target.equals("/favicon.ico")) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
+        System.out.print("GET:  ");
         System.out.println(target);
-        response.getWriter().println();
-        
 
+        if (target.equals("/")) {
+            // Here we serve the list of builds
+            String buildString = "<p>This is a build</p>";
+            buildString += "<p>This is another build</p>";
+            response.getWriter().println("<!DOCTYPE html><html><body><h1><a href=\"/\">CI Server</a></h1><p>This is the list of builds.</p>" + buildString + "</body></html>");
+        } else {
+            // Here we serve the information from a specific build
+            response.getWriter().println("<!DOCTYPE html><html><body><h1><a href=\"/\">CI Server</a></h1><p>This is build " + target + ".</p></body></html>");
+        }
+    }
+
+    /**
+     * This function is called for all incoming requests to the server.
+     * It handles incoming webhooks and runs the CI server, reporting back to
+     * GitHub. It also serves a web interface with information on the build status
+     * and test status of different tests.
+     */
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        if (baseRequest.getMethod().equals("GET"))
+            handleGet(target, baseRequest, request, response);
+        else
+            handlePost(target, baseRequest, request, response);
     }
 
     /**
