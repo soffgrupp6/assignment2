e3c9ff9d47ed31eda93c4703eac3e5dfc5728030
Daniel <danne_1998@hotmail.se>
2021-02-15 11:01:50 +0100

    add run script to make running easier, parse compilation output

--- assignment2/src/main/java/kth/soffgrupp/se/Compiler.java
+++ assignment2/src/main/java/kth/soffgrupp/se/Compiler.java
@@ -8,19 +8,24 @@ class Compiler {
      * The stdout and stderror of the compilation process is printed to the servers stdout.
      * In the case of an IOException, the exceptions stacktrace is printed to the servers stdout. 
      */
-    public void compile() {
+    public void compile() throws RuntimeException {
         try {
-            Process p = Runtime.getRuntime().exec("mvn clean compile assembly:single");
+            Process p = Runtime.getRuntime().exec("cd test/assignment2 && mvn clean compile assembly:single");
             BufferedReader stdin = new BufferedReader(new InputStreamReader(p.getInputStream()));
             BufferedReader stderror = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String s = null;
+            boolean success = false;
 
             while((s = stdin.readLine()) != null) {
+                if(s.contains("BUILD SUCCESS"))
+                    success = true;
                 System.out.println(s);
             }
             while((s = stderror.readLine()) != null) {
                 System.out.println(s);
             }
+            if(success == false)
+                throw new RuntimeException("Failed to compile");
             return;
         }
         catch(IOException e) {
