347738bb3b4c599bcab9377853a20860e33ebb04
astudic <58708697+astudic@users.noreply.github.com>
2021-02-16 10:21:21 +0100

    Implement Tester with logging (#22)
    Add self-testing functionality with BuildLogger class for logging the results. Consistency changes to Compiler class.
    
    Resolves #5

--- assignment2/src/main/java/kth/soffgrupp/se/Compiler.java
+++ assignment2/src/main/java/kth/soffgrupp/se/Compiler.java
@@ -8,23 +8,22 @@ class Compiler {
      * The stdout and stderror of the compilation process is printed to the servers stdout.
      * In the case of an IOException, the exceptions stacktrace is printed to the servers stdout. 
      */
-    public void compile() throws RuntimeException {
+    public void compile(BuildLogger log) throws RuntimeException {
         try {
             Process p = Runtime.getRuntime().exec("mvn -f test/assignment2 clean compile assembly:single");
             BufferedReader stdin = new BufferedReader(new InputStreamReader(p.getInputStream()));
             BufferedReader stderror = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String s = null;
-            boolean success = false;
 
             while((s = stdin.readLine()) != null) {
                 if(s.contains("BUILD SUCCESS"))
-                    success = true;
+                    log.compile_success = true;
                 System.out.println(s);
             }
             while((s = stderror.readLine()) != null) {
                 System.out.println(s);
             }
-            if(success == false)
+            if(!log.compile_success)
                 throw new RuntimeException("Failed to compile");
             return;
         }
--- assignment2/src/main/java/kth/soffgrupp/se/Tester.java
+++ assignment2/src/main/java/kth/soffgrupp/se/Tester.java
@@ -1,9 +1,69 @@
 package kth.soffgrupp.se;
 
-class Tester {
+import java.io.*;
 
-    public void test() {
-        
+class Tester {
+    /**
+     * The test method opens a new process to run the project tests.
+     * The stdout and stderror of the compilation process is printed to the servers stdout.
+     * In the case of an IOException, the exceptions stacktrace is printed to the servers stdout.
+     * The method writes the test results to the log object.
+     *
+     * @param log                 Object for logging the build
+     * @throws RuntimeException
+     */
+
+    public void test(BuildLogger log) throws RuntimeException{
+        try {
+            // Run tests
+            Process p = Runtime.getRuntime().exec("mvn -f test/assignment2 test");
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stderror = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String s;
+
+            while((s = stdin.readLine()) != null) {
+                //Check test output for results
+                System.out.println(s);
+                if (s.contains("Tests run") && s.contains("Failures")){
+                    int[] vars = parseStat(s, log);
+                    //Log test results
+                    log.tests_run = vars[0];
+                    log.tests_failed = vars[1];
+                    log.tests_errors = vars[2];
+                }
+                if (s.contains("BUILD SUCCESS")){
+                    log.test_success = true;
+                }
+            }
+            while((s = stderror.readLine()) != null) {
+                System.out.println(s);
+            }
+            //Throw exceptions at fail
+            if(log.tests_errors > 0 || !log.test_success) {
+                throw new RuntimeException("Error in tests");
+            }
+            if(log.tests_failed > 0){
+                throw new RuntimeException("Some tests failed");
+            }
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
-    
+
+    /**
+     * parseStat is a helper method used for parsing the test statistics for easier logging.
+     * @param s     String with test statistics
+     * @param log   Logging object
+     */
+
+    private int[] parseStat(String s, BuildLogger log){
+        String[] s_arr = s.split(",");
+        int[] vars = new int[3];
+        for (int i=0; i<3; i++){
+            String[] s_instance = s_arr[i].split(":");
+            vars[i] = Integer.parseInt(s_instance[1].strip());
+        }
+        return vars;
+    }
 }
