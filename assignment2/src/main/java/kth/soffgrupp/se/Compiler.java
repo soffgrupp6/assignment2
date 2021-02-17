2f27b837c0053481429888231cb2d4d1d7dc2fe9
Daniel <danne_1998@hotmail.se>
2021-02-17 12:25:19 +0100

    add test for compiler

--- assignment2/src/main/java/kth/soffgrupp/se/Compiler.java
+++ assignment2/src/main/java/kth/soffgrupp/se/Compiler.java
@@ -8,28 +8,26 @@ class Compiler {
      * The stdout and stderror of the compilation process is printed to the servers stdout.
      * In the case of an IOException, the exceptions stacktrace is printed to the servers stdout.
      */
-    public void compile(BuildLogger log) throws RuntimeException {
+    public void compile(BuildLogger log, String path) throws RuntimeException {
         try {
-            Process p = Runtime.getRuntime().exec("mvn -f test/assignment2 clean compile assembly:single");
+            Process p = Runtime.getRuntime().exec("mvn -f" + path + " clean compile assembly:single");
             BufferedReader stdin = new BufferedReader(new InputStreamReader(p.getInputStream()));
             BufferedReader stderror = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String s = null;
 
             while((s = stdin.readLine()) != null) {
-                if(s.contains("BUILD SUCCESS"))
+                if(s.contains("BUILD SUCCESS")){
                     log.setCompile_success(true);
-                System.out.println(s);
+                }
             }
             while((s = stderror.readLine()) != null) {
                 System.out.println(s);
             }
             if(!log.isCompile_success())
                 throw new RuntimeException("Failed to compile");
-            return;
         }
         catch(IOException e) {
             e.printStackTrace();
-            return;
         }
     }
 }
